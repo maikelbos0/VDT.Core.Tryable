e07d9921953d52a6d3e5411d3b17f45aa3d0f140
Maikel Bos <maikel.bos0@gmail.com>
2025-01-03 12:13:49 +0100

    Add result from handling errors

---
+++ src/VDT.Core.Tryable.Tests/ErrorHandlerResultTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace VDT.Core.Tryable.Tests;
+
+public class ErrorHandlerResultTests {
+    [Fact]
+    public void Skipped() {
+        var subject = ErrorHandlerResult<int>.Skipped;
+
+        Assert.False(subject.IsHandled);
+        Assert.Equal(0, subject.Value);
+    }
+
+    [Fact]
+    public void Handled() {
+        var subject = ErrorHandlerResult<int>.Handled(5);
+
+        Assert.True(subject.IsHandled);
+        Assert.Equal(5, subject.Value);
+    }
+}
---
+++ src/VDT.Core.Tryable/ErrorHandlerResult.cs
@@ -0,0 +1,7 @@
+﻿namespace VDT.Core.Tryable;
+
+public record ErrorHandlerResult<TValue> (bool IsHandled, TValue? Value) {
+    public static ErrorHandlerResult<TValue> Skipped => new(false, default);
+
+    public static ErrorHandlerResult<TValue> Handled(TValue value) => new(true, value);
+};
