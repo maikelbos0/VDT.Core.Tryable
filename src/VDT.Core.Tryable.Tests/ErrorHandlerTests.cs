ada0d4eb3456a987c453302eeae354806b0bd3de
Maikel Bos <maikel.bos0@gmail.com>
2025-01-03 12:31:04 +0100

    Add ErrorHandler concept

---
+++ src/VDT.Core.Tryable.Tests/ErrorHandlerTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Xunit;
+
+namespace VDT.Core.Tryable.Tests;
+
+public class ErrorHandlerTests {
+    [Fact]
+    public void DoesNotHandleWrongExceptionType() {
+        var subject = new ErrorHandler<InvalidOperationException, int>(ex => 10);
+
+        var result = subject.Handle(new Exception());
+
+        Assert.False(result.IsHandled);
+        Assert.Equal(0, result.Value);
+    }
+
+    [Fact]
+    public void DoesNotHandleWhenFilterReturnsFalse() {
+        var subject = new ErrorHandler<Exception, int>(ex => 10, ex => false);
+
+        var result = subject.Handle(new Exception());
+
+        Assert.False(result.IsHandled);
+        Assert.Equal(0, result.Value);
+    }
+
+    [Fact]
+    public void HandlesCorrectExceptionTypeWithoutFilter() {
+        var subject = new ErrorHandler<InvalidOperationException, int>(ex => 10);
+
+        var result = subject.Handle(new InvalidOperationException());
+
+        Assert.True(result.IsHandled);
+        Assert.Equal(10, result.Value);
+    }
+
+    [Fact]
+    public void HandlesCorrectExceptionTypeWhenFilterReturnsTrue() {
+        var subject = new ErrorHandler<Exception, int>(ex => 10, ex => true);
+
+        var result = subject.Handle(new Exception());
+
+        Assert.True(result.IsHandled);
+        Assert.Equal(10, result.Value);
+    }
+}
---
+++ src/VDT.Core.Tryable/ErrorHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace VDT.Core.Tryable;
+
+public class ErrorHandler<TException, TValue> where TException : Exception {
+    public Func<TException, TValue> Function { get; set; }
+    public Func<TException, bool>? Filter { get; set; }
+
+    public ErrorHandler(Func<TException, TValue> function) : this(function, null) { }
+
+    public ErrorHandler(Func<TException, TValue> function, Func<TException, bool>? filter) {
+        Function = function;
+        Filter = filter;
+    }
+
+    public ErrorHandlerResult<TValue> Handle(Exception ex) {
+        if (ex is not TException exception) {
+            return ErrorHandlerResult<TValue>.Skipped;
+        }
+
+        if (Filter != null && !Filter(exception)) {
+            return ErrorHandlerResult<TValue>.Skipped;
+        }
+
+        return ErrorHandlerResult<TValue>.Handled(Function(exception));
+    }
+}
