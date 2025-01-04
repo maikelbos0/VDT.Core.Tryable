e17e431af3b3b4a3b8e5b16d9da63d8906337b26
Maikel Bos <maikel.bos0@gmail.com>
2025-01-04 14:33:42 +0100

    Add bogus generated source

---
+++ src/VDT.Core.Tryable.Tests/GeneratedTryableTests.cs
@@ -0,0 +1,10 @@
+﻿using Xunit;
+
+namespace VDT.Core.Tryable.Tests;
+
+public class GeneratedTryableTests {
+    [Fact]
+    public void Test() {
+        var subject = new GeneratedTryable();
+    }
+}
