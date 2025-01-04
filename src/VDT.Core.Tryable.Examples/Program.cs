3d409e1c667d942fc893493ea4c8e935b2266f54
Maikel Bos <maikel.bos0@gmail.com>
2025-01-04 17:53:50 +0100

    Clean up

--- src/VDT.Core.Tryable.Examples/Program.cs
+++ src/VDT.Core.Tryable.Examples/Program.cs
@@ -5,8 +5,3 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
 await builder.Build().RunAsync();
-
-// TODO value generator method in stream ctor?
-// TODO - with option to replay method or values only
-// TODO error handling?
-// TODO complete?
