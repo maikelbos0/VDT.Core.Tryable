9db0e6ee38fec1561d5c25e84ca5037fc7df90cd
Maikel Bos <maikel.bos0@gmail.com>
2025-01-05 19:58:13 +0100

    Add extensions for builder pattern

---
+++ src/VDT.Core.Tryable.Tests/TryableExtensionsTests.cs
@@ -0,0 +1,17 @@
+﻿using NSubstitute;
+using System;
+using Xunit;
+
+namespace VDT.Core.Tryable.Tests;
+
+public class TryableExtensionsTests {
+    [Fact]
+    public void Catch_DefaultErrorHandler() {
+        var defaultErrorHandler = Substitute.For<Func<int>>();
+        var subject = new Tryable<int>(() => 5);
+
+        Assert.Equal(subject, subject.Catch(defaultErrorHandler));
+
+        Assert.Equal(defaultErrorHandler, subject.DefaultErrorHandler);
+    }
+}
---
+++ src/VDT.Core.Tryable/TryableExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace VDT.Core.Tryable;
+
+public static class TryableExtensions {
+    public static Tryable<TValue> Catch<TValue>(this Tryable<TValue> tryable, Func<TValue> defaultErrorHandler) {
+        tryable.DefaultErrorHandler = defaultErrorHandler;
+        return tryable;
+    }
+}
