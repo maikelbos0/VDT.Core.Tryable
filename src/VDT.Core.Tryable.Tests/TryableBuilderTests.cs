393316e6d8763b0fa1706cfea73278784d2cef62
Maikel Bos <maikel.bos0@gmail.com>
2025-01-05 19:57:48 +0100

    Add builder for starting a try

---
+++ src/VDT.Core.Tryable.Tests/TryableBuilderTests.cs
@@ -0,0 +1,16 @@
+﻿using NSubstitute;
+using System;
+using Xunit;
+using static VDT.Core.Tryable.TryableBuilder;
+
+namespace VDT.Core.Tryable.Tests;
+
+public class TryableBuilderTests {
+    [Fact]
+    public void TryCreatesTryable() {
+        var function = Substitute.For<Func<int>>();
+        var result = Try(function);
+
+        Assert.Equal(function, result.Function);
+    }
+}
---
+++ src/VDT.Core.Tryable/TryableBuilder.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace VDT.Core.Tryable;
+
+public static class TryableBuilder {
+    public static Tryable<TValue> Try<TValue>(Func<TValue> function) => new(function);
+}
