99beaa820278b4548c2c66f257e11e7c40da9e01
Maikel Bos <maikel.bos0@gmail.com>
2025-01-05 20:20:43 +0100

    Add error handler adding to builder extensions

--- src/VDT.Core.Tryable.Tests/TryableExtensionsTests.cs
+++ src/VDT.Core.Tryable.Tests/TryableExtensionsTests.cs
@@ -6,7 +6,44 @@ namespace VDT.Core.Tryable.Tests;
 
 public class TryableExtensionsTests {
     [Fact]
-    public void Catch_DefaultErrorHandler() {
+    public void CatchAddsErrorHandlerWithoutFilter() {
+        var handler = Substitute.For<Func<InvalidOperationException, int>>();
+        var subject = new Tryable<int>(() => 5) {
+            ErrorHandlers = {
+                Substitute.For<IErrorHandler<int>>()
+            }
+        };
+
+        Assert.Equal(subject, subject.Catch(handler));
+
+        Assert.Equal(2, subject.ErrorHandlers.Count);
+
+        var errorHandler = Assert.IsType<ErrorHandler<InvalidOperationException, int>>(subject.ErrorHandlers[1]);
+        Assert.Equal(handler, errorHandler.Handler);
+        Assert.Null(errorHandler.Filter);
+    }
+
+    [Fact]
+    public void CatchAddsErrorHandlerWithFilter() {
+        var handler = Substitute.For<Func<InvalidOperationException, int>>();
+        var filter = Substitute.For<Func<InvalidOperationException, bool>>();
+        var subject = new Tryable<int>(() => 5) {
+            ErrorHandlers = {
+                Substitute.For<IErrorHandler<int>>()
+            }
+        };
+
+        Assert.Equal(subject, subject.Catch(handler, filter));
+
+        Assert.Equal(2, subject.ErrorHandlers.Count);
+
+        var errorHandler = Assert.IsType<ErrorHandler<InvalidOperationException, int>>(subject.ErrorHandlers[1]);
+        Assert.Equal(handler, errorHandler.Handler);
+        Assert.Equal(filter, errorHandler.Filter);
+    }
+
+    [Fact]
+    public void CatchAddsDefaultErrorHandler() {
         var defaultErrorHandler = Substitute.For<Func<int>>();
         var subject = new Tryable<int>(() => 5);
 
--- src/VDT.Core.Tryable/TryableExtensions.cs
+++ src/VDT.Core.Tryable/TryableExtensions.cs
@@ -3,6 +3,16 @@
 namespace VDT.Core.Tryable;
 
 public static class TryableExtensions {
+    public static Tryable<TValue> Catch<TException, TValue>(this Tryable<TValue> tryable, Func<TException, TValue> handler) where TException : Exception {
+        tryable.ErrorHandlers.Add(new ErrorHandler<TException, TValue>(handler));
+        return tryable;
+    }
+
+    public static Tryable<TValue> Catch<TException, TValue>(this Tryable<TValue> tryable, Func<TException, TValue> handler, Func<TException, bool> filter) where TException : Exception {
+        tryable.ErrorHandlers.Add(new ErrorHandler<TException, TValue>(handler, filter));
+        return tryable;
+    }
+
     public static Tryable<TValue> Catch<TValue>(this Tryable<TValue> tryable, Func<TValue> defaultErrorHandler) {
         tryable.DefaultErrorHandler = defaultErrorHandler;
         return tryable;
