252d5101a36568b6ed1a19abc5672638bf5f87bc
Maikel Bos <maikel.bos0@gmail.com>
2025-01-13 20:40:46 +0100

    Move builder methods to base

---
+++ src/VDT.Core.Tryable.Tests/TryableBaseTests.cs
@@ -0,0 +1,70 @@
+﻿using NSubstitute;
+using System;
+using Xunit;
+
+namespace VDT.Core.Tryable.Tests;
+
+public class TryableBaseTests {
+    public class TestTryable : TryableBase<int, Action, TestTryable> {
+        public TestTryable() : base(() => throw new NotImplementedException()) { }
+
+        public override int Execute() => throw new NotImplementedException();
+    }
+
+    [Fact]
+    public void CatchAddsErrorHandlerWithoutFilter() {
+        var handler = Substitute.For<Func<InvalidOperationException, int>>();
+        var subject = new TestTryable() {
+            ErrorHandlers = {
+                Substitute.For<IErrorHandler<int>>()
+            }
+        };
+
+        Assert.Equal(subject, subject.Catch(handler));
+
+        Assert.Equal(2, subject.ErrorHandlers.Count);
+
+        var errorHandler = Assert.IsType<ErrorHandler<InvalidOperationException, int>>(subject.ErrorHandlers[1]);
+        Assert.Equal(handler, errorHandler.Handler);
+        Assert.Null(errorHandler.Filter);
+    }
+
+    [Fact]
+    public void CatchAddsErrorHandlerWithFilter() {
+        var handler = Substitute.For<Func<InvalidOperationException, int>>();
+        var filter = Substitute.For<Func<InvalidOperationException, bool>>();
+        var subject = new TestTryable() {
+            ErrorHandlers = {
+                Substitute.For<IErrorHandler<int>>()
+            }
+        };
+
+        Assert.Equal(subject, subject.Catch(filter, handler));
+
+        Assert.Equal(2, subject.ErrorHandlers.Count);
+
+        var errorHandler = Assert.IsType<ErrorHandler<InvalidOperationException, int>>(subject.ErrorHandlers[1]);
+        Assert.Equal(handler, errorHandler.Handler);
+        Assert.Equal(filter, errorHandler.Filter);
+    }
+
+    [Fact]
+    public void CatchAddsDefaultErrorHandler() {
+        var defaultErrorHandler = Substitute.For<Func<int>>();
+        var subject = new TestTryable();
+
+        Assert.Equal(subject, subject.Catch(defaultErrorHandler));
+
+        Assert.Equal(defaultErrorHandler, subject.DefaultErrorHandler);
+    }
+
+    [Fact]
+    public void Finally() {
+        var completeHandler = Substitute.For<Action>();
+        var subject = new TestTryable();
+
+        Assert.Equal(subject, subject.Finally(completeHandler));
+
+        Assert.Equal(completeHandler, subject.CompleteHandler);
+    }
+}
--- src/VDT.Core.Tryable/Tryable.cs
+++ src/VDT.Core.Tryable/Tryable.cs
@@ -2,31 +2,11 @@
 
 namespace VDT.Core.Tryable;
 
-public class Tryable<TValue> : TryableBase<TValue, Action> {
+public class Tryable<TValue> : TryableBase<TValue, Action, Tryable<TValue>> {
     public static implicit operator TValue(Tryable<TValue> tryable) => tryable.Execute();
 
     public Tryable(Func<TValue> function) : base(function) {
         Function = function;
-    }
-
-    public Tryable<TValue> Catch<TException>(Func<TException, TValue> handler) where TException : Exception {
-        ErrorHandlers.Add(new ErrorHandler<TException, TValue>(handler));
-        return this;
-    }
-
-    public Tryable<TValue> Catch<TException>(Func<TException, bool> filter, Func<TException, TValue> handler) where TException : Exception {
-        ErrorHandlers.Add(new ErrorHandler<TException, TValue>(filter, handler));
-        return this;
-    }
-
-    public Tryable<TValue> Catch(Func<TValue> defaultErrorHandler) {
-        DefaultErrorHandler = defaultErrorHandler;
-        return this;
-    }
-
-    public Tryable<TValue> Finally(Action completeHandler) {
-        CompleteHandler = completeHandler;
-        return this;
     }
 
     public override TValue Execute() {
